592691f2c316be1fd36b4467c64162886051f260
Sherman-m <Sherman-88@yandex.ru>
2024-04-10 19:52:46 +0300

    added currencies

---
+++ src/trading/currency.hpp
@@ -0,0 +1,25 @@
+#ifndef CURRENCY_HPP
+#define CURRENCY_HPP
+
+#include "../util/tagged_numeric.hpp"
+#include <cinttypes>
+
+namespace currency {
+
+namespace detail {
+
+using CurrencyNumericType = int64_t;
+
+template <typename Tag>
+using Currency = util::TaggedNumeric<CurrencyNumericType, Tag>;
+
+}  // namespace detail
+
+struct UsdTag {};
+using Usd = detail::Currency<UsdTag>;
+
+struct RublesTag {};
+using Rubles = detail::Currency<RublesTag>;
+
+}  // namespace currency
+#endif
